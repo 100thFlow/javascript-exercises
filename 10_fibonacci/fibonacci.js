baf57c1185ecdf7279fb4e9e36e25c1a6f87fefc
100thFlow <100thFlow@proton.me>
2025-02-27 00:35:24 +0200

    Complete 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = +n;
+  if (n === 0) return 0;
+  else if (n < 2 && n > 0) return 1;
+  else if (n < 0) return 'OOPS';
 
+  let fibonacciSequence = [1, 1];
+  let fibonacciNext;
+  for (i = 3; i <= n; ++i) {
+    fibonacciNext = fibonacciSequence.at(-1) + fibonacciSequence.at(-2);
+    fibonacciSequence.push(fibonacciNext);
+  }
+  return fibonacciSequence.at(-1);
 };
 
 // Do not edit below this line
