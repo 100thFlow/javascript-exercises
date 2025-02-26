1db796c21f73f6ac2d482dceb86b30a0369b32cc
100thFlow <100thFlow@proton.me>
2025-02-26 23:24:58 +0200

    Complete 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  formattedString = string.replace(/[!"#$%&'()*+ ,-./:;<=>?@[\]^_`{|}~]/g, '');
+  if (
+    formattedString.split('').reverse().join('').toLowerCase() ===
+    formattedString.toLowerCase()
+  ) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
