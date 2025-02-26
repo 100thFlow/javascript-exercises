d58d4a7185422ec29367af55cfe2eaa721d13454
100thFlow <100thFlow@proton.me>
2025-02-26 19:45:31 +0200

    Complete 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (from, to) {
+  if (
+    [from, to].some(
+      (value) =>
+        typeof value !== 'number' || !Number.isInteger(value) || value < 0,
+    )
+  ) {
+    return 'ERROR';
+  }
+  let result = 0;
+  let minNumber = Math.min(from, to);
+  let maxNumber = Math.max(from, to);
+  for (; minNumber <= maxNumber; ++minNumber) {
+    result += minNumber;
+  }
+  return result;
 };
 
 // Do not edit below this line
