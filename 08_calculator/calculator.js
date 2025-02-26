7e3df289d40af67a9b293dc744eae2fe2552c972
100thFlow <100thFlow@proton.me>
2025-02-26 23:02:15 +0200

    Complete 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let sum = 0;
+
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (numbers) {
+  let result = 1;
 
+  for (let number of numbers) {
+    result *= number;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let result = 1;
+  for (let multiplier = number; multiplier > 0; --multiplier) {
+    result *= multiplier;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
