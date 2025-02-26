0d9b2f13ddaf82583b67a33eec0374266349088e
100thFlow <100thFlow@proton.me>
2025-02-26 19:08:49 +0200

    Complete 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elemets) {
+  for (let i = 0; i < elemets.length; ++i) {
+    let index = array.indexOf(elemets[i]);
+    while (index != -1) {
+      array.splice(index, 1);
+      index = array.indexOf(elemets[i]);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
