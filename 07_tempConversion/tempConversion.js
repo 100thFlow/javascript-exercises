07f821d27de91f83898d34c0127313f73c5cba43
100thFlow <100thFlow@proton.me>
2025-02-26 22:32:54 +0200

    Complete 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  let result = ((fahr - 32) * 5) / 9;
+  if (result % 2 !== 0 && result !== 0) {
+    result = +result.toFixed(1);
+  }
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  result = (cel * 9) / 5 + 32;
+  if (result % 2 !== 0 && result !== 0) {
+    result = +result.toFixed(1);
+  }
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
