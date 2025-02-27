d353ace674b48f48227e7b31ff85803bc186e5e1
100thFlow <100thFlow@proton.me>
2025-02-27 15:07:07 +0200

    Complete 12 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  if (array.length === 0) return 'ERROR';
+  return array.reduce((oldestPerson, person) => {
+    const currentDate = new Date();
+    const getAge = (person) =>
+      (person.yearOfDeath ?? currentDate.getFullYear()) - person.yearOfBirth;
+    return getAge(person) > getAge(oldestPerson) ? person : oldestPerson;
+  });
 };
 
 // Do not edit below this line
